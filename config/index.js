03407be447bf291c934025179b45cbd773981210
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-29 13:58:17 +0200

    Updates base urls

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@
 
 var config = {
   SERVER_PORT: process.env.SERVER_PORT || 3000,
-  BASE_URL: process.env.BASE_URL || 'http://www.telemark.no',
-  SITEMAP_URL: process.env.SITEMAP_URL || 'http://www.telemark.no/sitemap.xml',
+  BASE_URL: process.env.BASE_URL || 'https://www.telemark.no',
+  SITEMAP_URL: process.env.SITEMAP_URL || 'https://www.telemark.no/sitemap.xml',
   YAR_SECRET: process.env.YAR_SECRET || 'passwordpasswordpasswordpassword'
 }
 
