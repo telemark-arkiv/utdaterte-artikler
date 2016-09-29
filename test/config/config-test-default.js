d9fb6f819b1bd6ae480bddd4c3430b621224928c
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-29 14:00:49 +0200

    Implements nsp

--- test/config/config-test-default.js
+++ test/config/config-test-default.js
@@ -4,6 +4,6 @@ var tap = require('tap')
 var config = require('../../config/index')
 
 tap.equal(config.SERVER_PORT, 3000, 'Default SERVER_PORT ok')
-tap.equal(config.BASE_URL, 'http://www.telemark.no', 'Default BASE_URL ok')
-tap.equal(config.SITEMAP_URL, 'http://www.telemark.no/sitemap.xml', 'Default SITEMAP_URL ok')
+tap.equal(config.BASE_URL, 'https://www.telemark.no', 'Default BASE_URL ok')
+tap.equal(config.SITEMAP_URL, 'https://www.telemark.no/sitemap.xml', 'Default SITEMAP_URL ok')
 tap.equal(config.YAR_SECRET, 'passwordpasswordpasswordpassword', 'Default YAR_SECRET ok')
